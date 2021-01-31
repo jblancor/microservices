dadbed5a3e433f10a1cc1bfe87698b52919ba03a
jose <jose.blanco.rodriguez@ricoh.es>
2021-01-31 20:45:43 +0100

    'Microservices'

---
+++ api-gateway/src/main/java/org/jb/formacion/microdemo/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package org.jb.formacion.microdemo.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
